df3e66bf318e0a559f7c1cb6f422e359b89ce8bd
OscarCrs <zcrscortez@gmail.com>
2026-07-04 17:47:36 -0600

    fue la implementacion de CargarProductos(), lo que hace es mostrar la base de datos en la ventana

--- Forms/frmProductos.cs
+++ Forms/frmProductos.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+using System;
 using System.Windows.Forms;
 
 namespace Punto.Forms
@@ -7,6 +9,48 @@ namespace Punto.Forms
         public frmProductos()
         {
             InitializeComponent();
+        }
+
+        private void frmProductos_Load(object sender, System.EventArgs e)
+        {
+            CargarProductos();
+        }
+        private void CargarProductos()
+        {
+            Conexion conClase = new Conexion();
+            MySqlConnection dbConn = conClase.GetConeccion();
+
+            if (dbConn == null)
+            {
+                return;
+            }
+            try
+            {
+                string query = "SELECT producto_id, codigo, descripcion, precio, stock FROM productos";
+
+                using (MySqlCommand comando = new MySqlCommand(query, dbConn))
+                {
+                    using (MySqlDataAdapter adaptador = new MySqlDataAdapter(comando))
+                    {
+                        System.Data.DataTable dt = new System.Data.DataTable();
+                        adaptador.Fill(dt);
+                        dgvProductos.DataSource = dt;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los productos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                dbConn.Close();
+            }
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
