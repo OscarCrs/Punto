e9a5f03500266b232bdd123200e59f19e0617a36
OscarCrs <zcrscortez@gmail.com>
2026-07-04 11:56:09 -0600

    Creacion de la clase conexión.cs con try-catch

---
+++ Forms/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto.Forms
+{
+    internal class Conexion
+    {
+        //Cadena de conexion a la base de datos con los 5 parametros
+        private readonly string Cadena;
+
+        //constructor
+        public Conexion()
+        {
+            Cadena = "Server=127.0.0.1; Database=PuntoDB; Uid=root; Pwd=; Port=3306;";
+        }
+
+        //metodo para conectar a la base de datos
+        public MySqlConnection GetConeccion()
+        {
+            try
+            {
+                MySqlConnection con = new MySqlConnection(Cadena);
+                con.Open();
+                MessageBox.Show("Conexion Exitosa...");
+                return con;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error al conectarse con la base de datos \n"+ ex.Message);
+                return null;
+            }
+
+        }
+    }
+}
